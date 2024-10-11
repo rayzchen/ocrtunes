818cdced33887f1bbf1c6f75760d6585d2c69a26
rayzchen <tankimarshal2@gmail.com>
2024-10-11 11:05:06 +0000

    Move database in schema into package

--- ocrtunes/database.py
+++ ocrtunes/database.py
@@ -1,16 +1,22 @@
 import sqlite3
 import os
 
+directory = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "database"
+)
+database_file = os.path.join(directory, "ocrtunes.db")
+schema_file = os.path.join(directory, "schema.sql")
 
 def initialise_db():
     # Check if datbase file exists
-    if os.path.isfile("ocrtunes.db"):
+    if os.path.isfile(database_file):
         # Load database file
-        db = sqlite3.connect("ocrtunes.db")
+        db = sqlite3.connect(database_file)
         return db
     else:
         # Database file not found, generating via schema
-        db = sqlite3.connect("ocrtunes.db")
-        with open("schema.sql") as f:
+        db = sqlite3.connect(database_file)
+        with open(schema_file) as f:
             db.executescript(f.read())
         return db
